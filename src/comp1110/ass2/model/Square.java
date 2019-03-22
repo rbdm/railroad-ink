675130e314d4df2fc67c790dcf5c1ade74636603
rbdm <mirhadydorodjatun@gmail.com>
2019-03-22 21:42:53 +1100

    Changed Boolean isStation of TypeSquare to isOverpass

--- src/comp1110/ass2/model/Square.java
+++ src/comp1110/ass2/model/Square.java
@@ -9,17 +9,17 @@ public class Square
     public TypeTile right;
     public TypeTile bottom;
     public TypeTile left;
-    public Boolean isStation;
+    public Boolean isOverpass;
     public String name;
     public TypeSquare type;
 
-    public Square(TypeTile top, TypeTile right, TypeTile bottom, TypeTile left, Boolean isStation, String name,TypeSquare type)
+    public Square(TypeTile top, TypeTile right, TypeTile bottom, TypeTile left, Boolean isOverpass, String name,TypeSquare type)
     {
         this.top = top;
         this.right = right;
         this.bottom = bottom;
         this.left = left;
-        this.isStation = isStation;
+        this.isOverpass = isOverpass;
         this.name = name;
         this.type = type;
     }
--- src/comp1110/ass2/model/TypeTile.java
+++ src/comp1110/ass2/model/TypeTile.java
@@ -3,6 +3,5 @@ package comp1110.ass2.model;
 public enum TypeTile {
     RAILWAY,
     HIGHWAY,
-    STATION,
     BLOCK
 }
