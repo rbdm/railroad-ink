4a9e766f786cd34b54bbcac2a88997d59cd43fe7
Yue ZHANG <u6797258@anu.edu.au>
2019-03-29 13:24:17 +1100

    edit test

--- src/comp1110/ass2/Test.java
+++ src/comp1110/ass2/Test.java
@@ -1,6 +1,8 @@
 package comp1110.ass2;
 
 import comp1110.ass2.model.Board;
+import comp1110.ass2.model.Square;
+import comp1110.ass2.model.Tile;
 
 /**
  * User: u6613739
@@ -13,7 +15,7 @@ public class Test
     public static void main(String[] args)
     {
         Board board = new Board();
-        board.printMap();
-        board.getSquareFormSquareString("B3C52");
+        Tile tile = new Tile();
+        board.isValidPlacement(tile.getSquareList('A').get(0));
     }
 }
