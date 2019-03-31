64e90600dad4e0413ab8c95b298779e27a854e28
ChangZhehao <changzhehao@gmail.com>
2019-03-31 19:13:42 +1100

    update @Test change Test class to OurTest class

--- src/comp1110/ass2/OurTest.java
+++ src/comp1110/ass2/OurTest.java
@@ -1,30 +1,26 @@
 package comp1110.ass2;
 
-import comp1110.ass2.gui.Viewer;
 import comp1110.ass2.model.Board;
-import comp1110.ass2.model.Square;
-import comp1110.ass2.model.Tile;
-import comp1110.ass2.util.RouteUtil;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
 
 /**
  * User: u6613739
- * Date: 2019/3/22
- * Time: 20:05
+ * Date: 2019/3/31
+ * Time: 19:11
  * Description:
  */
-public class Test
+public class OurTest
 {
-    public static void main(String[] args)
-    {
-        testTask12();
-    }
-
-    public static void testTask12()
+//    @Rule
+//    public Timeout globalTimeout = Timeout.millis(2000);
+    @Test
+    public void testTask12()
     {
         Board board = new Board();
         board.putPlacementStringToMap("A4A10A1A30B2A40A1B01B2B10A1B21S1B32B1B44A4C10A5C31A4C41A5C53A4D01A3D13A5D23A3D50A4D61A4E20B1E31S2E40S4E50A1F01A2F11B1F24A1F30A0F51A1F61B0G12A2G30A1G41B1G52");
         //board.putPlacementStringToMap("A4A10B2B10");
         board.getBonusScoring();
-
     }
 }
