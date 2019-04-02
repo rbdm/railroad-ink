be6f6640f5627b676afc6adff42b09b662a9797c
Yue ZHANG <u6797258@anu.edu.au>
2019-04-02 11:55:17 +1100

    fixed allRoute

--- src/comp1110/ass2/util/ExitUtil.java
+++ src/comp1110/ass2/util/ExitUtil.java
@@ -8,6 +8,7 @@ import comp1110.ass2.model.TypeSquare;
 import comp1110.ass2.model.TypeTile;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ExitUtil {
@@ -17,9 +18,11 @@ public class ExitUtil {
 
         board.putPlacementStringToMap("A3D61A3D53B0C52A0B52A2B63A4D41B0E60A0F61A3D31A3D23A2G30B0F34A3E32A1B01B2B10A1B21A0A63A4D01A1G41B0G12S2D10A4C10B2A10A2B33A1A30S4E11A4E21A3C21A3C31S5F11");
         Square[][] map = board.getMap();
-        //List<Square> X = ExitUtil.getConnectedExit(map[0][2],map);
+        List<Square> X = ExitUtil.getConnectedExit(map[2][0],map);
+        int xx=getExitScore(map);
+        System.out.println(xx);
         //List<Square> X = ExitUtil.getConnectedNeighbour(map[2][2],map,TypeTile.BLOCK);
-        List<Square> X = ExitUtil.allRoute(map[1][2], map, map[0][2], new ArrayList<>());
+        //List<Square> X = ExitUtil.allRoute(map[1][2], map, map[0][2], new ArrayList<>());
         //System.out.println(conType(map[2][2],map[2][1]));
         //System.out.println(map[0][2].positionPoint.getY());
         for (Square i : X) {
@@ -28,6 +31,43 @@ public class ExitUtil {
         System.out.println();
     }
 
+    /**
+     *
+     * @param map
+     * @return
+     */
+
+    public static int getExitScore(Square[][] map){
+        List<Square> exitList =new ArrayList<>();
+        int Score=0;
+        for (int i=0;i<map.length;i++){
+            for (int j=0;j<map[0].length;j++){
+                if (map[i][j].type==TypeSquare.EXIT){
+                    exitList.add(map[i][j]);
+                }
+            }
+        }
+        return Score;
+
+    }
+
+    public static int exitScoreTable(int number){
+        switch (number){
+            case 2: return 4 ;
+            case 3:return 8;
+            case 4:return 12;
+            case 5:return 16;
+            case 6:return 20;
+            case 7:return 24;
+            case 8:return 28;
+            case 9:return 32;
+            case 10:return 36;
+            case 11:return 40;
+            case 12:return 45;
+            default:return 0;
+        }
+
+    }
     /**
      * input an exit A and return all other exit that are on the same mix-type route with A
      *
@@ -140,16 +180,24 @@ public class ExitUtil {
     }
 
     public static TypeTile conType(Square A, Square B) {
-        if (areConSquares(A, B) == false) {
+        if (areConSquares(A, B) == 'n') {
             return null;
         } else {
-            if ((A.left == B.right && A.left == TypeTile.RAILWAY) ||
-                    (A.right == B.left && A.right == TypeTile.RAILWAY) ||
-                    (A.bottom == B.top && A.bottom == TypeTile.RAILWAY) ||
-                    (A.top == B.bottom && A.top == TypeTile.RAILWAY)) {
-                return TypeTile.RAILWAY;
-            } else {
-                return TypeTile.HIGHWAY;
+            if (areConSquares(A,B)=='l'){
+                if (A.right==TypeTile.RAILWAY){return TypeTile.RAILWAY;}
+                else {return TypeTile.HIGHWAY;}
+            }
+            else if (areConSquares(A,B)=='r'){
+                if (A.left==TypeTile.RAILWAY){return TypeTile.RAILWAY;}
+                else {return TypeTile.HIGHWAY;}
+            }
+            else if (areConSquares(A,B)=='a'){
+                if (A.bottom==TypeTile.RAILWAY){return TypeTile.RAILWAY;}
+                else {return TypeTile.HIGHWAY;}
+            }
+            else {
+                if (A.top==TypeTile.RAILWAY){return TypeTile.RAILWAY;}
+                else {return TypeTile.HIGHWAY;}
             }
         }
     }
@@ -158,13 +206,16 @@ public class ExitUtil {
     public static List<Square> allRoute(Square s, Square[][] map, Square lastIn, List<Square> alreadyGet)
     {
         List<Square> answer = new ArrayList<>();
-        if (alreadyGet.contains(s) == false) {
-            alreadyGet.add(s);
+        Square squareNow=s;
+        Square squareLast = lastIn;
+
+        if (alreadyGet.contains(squareNow) == false) {
+            alreadyGet.add(squareNow);
         }
-        if (alreadyGet.contains(lastIn) == false) {
-            alreadyGet.add(lastIn);
+        if (alreadyGet.contains(squareLast) == false) {
+            alreadyGet.add(squareLast);
         }
-        List<Square> neighbour = getConnectedNeighbour(s, map, conType(lastIn,s));
+        List<Square> neighbour = getConnectedNeighbour(squareNow, map, conType(squareLast,squareNow));
         List<Square> removeList = new ArrayList<>();
         for (Square i : neighbour) {
             if (alreadyGet.contains(i) == true) {
@@ -173,11 +224,12 @@ public class ExitUtil {
         }
         neighbour.removeAll(removeList);
         if (neighbour.isEmpty() == true) {
-            answer = alreadyGet;
+            return alreadyGet;
         } else{
             for (Square element : neighbour){
-                alreadyGet.add(element);
-                for(Square i:allRoute(element,map,s,alreadyGet)){
+                squareLast=squareNow;
+                squareNow=element;
+                for(Square i:allRoute(squareNow,map,squareLast,alreadyGet)){
                     if (answer.contains(i)==false){answer.add(i);}
                 }
             }
@@ -188,7 +240,7 @@ public class ExitUtil {
 
 
 
-    public static boolean areConSquares(Square A, Square B) {
+    public static char areConSquares(Square A, Square B) {
         int rowA = A.positionPoint.getX();
         int colA = A.positionPoint.getY();
         int rowB = B.positionPoint.getX();
@@ -197,32 +249,32 @@ public class ExitUtil {
 
         if (rowA == rowB && colB - colA == 1) { //A is on the left of B
             if (A.right == B.left && A.right != TypeTile.BLOCK) {
-                return true;
+                return 'l';
             } else {
-                return false;
+                return 'n';
             }
 
         } else if (rowA == rowB && colA - colB == 1) { //A is on the right of B
             if (A.left == B.right && A.left != TypeTile.BLOCK) {
-                return true;
+                return 'r';
             } else {
-                return false;
+                return 'n';
             }
 
         } else if (colA == colB && rowA - rowB == 1) { //A is below B
             if (A.top == B.bottom && A.top != TypeTile.BLOCK) {
-                return true;
+                return 'b';
             } else {
-                return false;
+                return 'n';
             }
         } else if (colA == colB && rowB - rowA == 1) {  //A is above B
             if (A.bottom == B.top && A.bottom != TypeTile.BLOCK) {
-                return true;
+                return 'a';
             } else {
-                return false;
+                return 'n';
             }
         } else {
-            return false;
+            return 'n';
         }
     }
 
