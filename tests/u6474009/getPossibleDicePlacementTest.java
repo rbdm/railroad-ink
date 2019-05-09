cfd231d00ab49b75bfcab7d348cf371ae8cbb4e9
rbdm <mirhadydorodjatun@gmail.com>
2019-05-09 13:44:51 +1000

    Added javadoc for getPossibleDicePlacementTest

--- tests/u6474009/getPossibleDicePlacementTest.java
+++ tests/u6474009/getPossibleDicePlacementTest.java
@@ -9,6 +9,9 @@ import static org.junit.Assert.*;
 
 public class getPossibleDicePlacementTest {
 
+    /**
+     * Checks the output for some regular combination of boards and dices
+     */
     @Test
     public void testNormalBoards() {
         int resultIndex = 0;
@@ -27,6 +30,7 @@ public class getPossibleDicePlacementTest {
      */
     @Test
     public void testFullBoard() {
+        String FULL_BOARD_STRING = "A0F00A0B00A0A31B1A14A0B61A0F61A0G32B1D61A0G43A0A62A0E61B1G56S1G60A0E03A0A03B1G12A0G02S0A50A0B50A5D03B1B40A0A40";
         for (String d : VALID_DICES) {
             Board board = new Board();
             board.putPlacementStringToMap(FULL_BOARD_STRING);
@@ -34,7 +38,9 @@ public class getPossibleDicePlacementTest {
         }
     }
 
-
+    /**
+     * Checks if the right exception is thrown for illegal input.
+     */
     @Test (expected = IllegalArgumentException.class)
     public void testIllegalDice() {
         for (String bs : NORMAL_BOARD_STRINGS) {
@@ -46,13 +52,11 @@ public class getPossibleDicePlacementTest {
         }
     }
 
+    // uses 2 board condition: regularly filled and empty board
     private String[] NORMAL_BOARD_STRINGS = {
             "A3D53A0B52A2B63A4D41B0E60A0F61A3D31A3D23A2G30B0F34A3E32A1B01B2B10A1B21A0A63A4D01A1G41B0G12S2D10A4C10B2A10A2B33B0A30S4E11A4E21A3C21",
             ""
     };
-
-    private String FULL_BOARD_STRING = "A0F00A0B00A0A31B1A14A0B61A0F61A0G32B1D61A0G43A0A62A0E61B1G56S1G60A0E03A0A03B1G12A0G02S0A50A0B50A5D03B1B40A0A40";
-
 
     private String[] VALID_DICES = {
             "A0",
